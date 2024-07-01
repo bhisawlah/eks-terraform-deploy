4083516caae997285db0f449075ae43d14408e00
bhisawlah <ajisegbedeabisolat@gmail.com>
2024-07-01 17:58:11 +0100

    bucket name update

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "infra/terraform.tfstate"
-    bucket         = "bhisawlah-terraform-backend-bucket"
+    bucket         = "bhisawlah2-terraform-backend-bucket"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
