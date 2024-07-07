729e19dd2b3ad1aa0c55f29f19c5a52bf1c8d65c
bhisawlah <ajisegbedeabisolat@gmail.com>
2024-07-07 15:10:07 +0100

    auth uncommented

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -59,16 +59,16 @@ module "eks" {
   }
 
   # aws-auth configmap
-  # manage_aws_auth_configmap = true
-  # create_aws_auth_configmap = true
+  manage_aws_auth_configmap = true
+  create_aws_auth_configmap = true
 
-  # aws_auth_roles = [
-  #   {
-  #     rolearn  = var.rolearn
-  #     username = "Bisola"
-  #     groups   = ["system:masters"]
-  #   },
-  # ]
+  aws_auth_roles = [
+    {
+      rolearn  = var.rolearn
+      username = "Bisola"
+      groups   = ["system:masters"]
+    },
+  ]
 
   tags = {
     env       = "dev"
