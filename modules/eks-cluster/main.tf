22e05c0cced6325c6a95ecfdbf332c94f219297a
bhisawlah <ajisegbedeabisolat@gmail.com>
2024-07-07 13:46:08 +0100

    auth commented update

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -59,8 +59,8 @@ module "eks" {
   }
 
   # aws-auth configmap
-  manage_aws_auth_configmap = true
-  create_aws_auth_configmap = true
+  # manage_aws_auth_configmap = true
+  # create_aws_auth_configmap = true
 
   # aws_auth_roles = [
   #   {
