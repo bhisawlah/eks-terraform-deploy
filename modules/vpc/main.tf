1aa1ef9ce5803b88a7a5c49106940ec26889bf56
bhisawlah <ajisegbedeabisolat@gmail.com>
2024-07-10 12:19:39 +0100

    subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -10,11 +10,11 @@ module "vpc" {
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
 
-  azs = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  azs = ["us-west-1a", "us-west-1c"]
   #private_subnets     = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
   #public_subnets      = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/24",  "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24",  "10.0.103.0/24"]
 
   enable_nat_gateway = true
 
